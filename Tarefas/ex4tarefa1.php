4194c14e85f7365d3660dfb03149d17458f48d40
Rita Alamino <ritaalamino@gmail.com>
2020-11-17 12:46:22 -0300

    insert npm

--- Tarefas/ex4tarefa1.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    $vetor = array('Joana' => "Estudante",'Julia' => "Estudante",'Flávia' => "Professora" );
-    foreach ($vetor as $nome => $tipo) {
-        echo "A " . $nome . " é uma " . $tipo . ".\n";
-    }
-?>
